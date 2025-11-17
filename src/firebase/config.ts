0b4eb6789f8ddfb2004199d06b3908d6da401709
Àngel Domènech <angelds97@gmail.com>
2025-11-17 18:01:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5157424802-d42cf",
+  "appId": "1:602570520102:web:ed0ab336057e776c6a98e7",
+  "apiKey": "AIzaSyB28REBXB7IWY4Z8YxcxOrs8JlzWntc7ss",
+  "authDomain": "studio-5157424802-d42cf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "602570520102"
+};
