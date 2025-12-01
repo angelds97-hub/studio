68410090e535b4af524b15a7203d480c59069d34
Àngel Domènech <angelds97@gmail.com>
2025-12-01 10:55:21 +0000

    tinc un xatbot: <!--Start of Tawk.to Script--> <script type="text/javas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Providers } from '@/components/layout/providers';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -40,6 +41,19 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <Providers>{children}</Providers>
         <Toaster />
+        <Script id="tawk-to-script" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/692d73b50962891980a79253/1jbconvno';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
